88d0fb7b134a2873c097bc1864c01d427d7bd395
Uttambg <115449178+Uttambg@users.noreply.github.com>
2024-09-12 17:54:22 +0530

    Update App.tsx
    changes done via github on dev

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I made some changes
-        </p>
+          I made some change in github
         <a
           className="App-link"
           href="https://reactjs.org"
